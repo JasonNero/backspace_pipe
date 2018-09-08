597c8019467dadba85231a5c4e5928fa21cdabc3
JasonNero <34405484+JasonNero@users.noreply.github.com>
2018-09-08 16:19:49 +0200

    added shelf and installer

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # backspace_pipe
-Installation is as simple as this:
-double click pipe_installer.bat
+## Installation/Updates
+### At home
+Save the pipe folder anywhere you like and execute pipe_install.bat
+
+### On Server
+Save the pipe folder on a network drive and let each user execute pipe_install.bat
--- pipe_installer.py
+++ pipe_installer.py
@@ -1,22 +1,23 @@
 import os
 import time
+import shutil
 
-scripts_path = os.getcwd().replace("\\", "\\\\")
-venv_path = scripts_path + r"\\backspace_venv\\Lib\\site-packages"
+pipe_path = os.getcwd().replace("\\", "\\\\")
+venv_path = pipe_path + r"\\backspace_venv\\Lib\\site-packages"
+
 usersetup_path = os.path.expanduser(r"~\\Documents\\maya\\2018\\scripts\\usersetup.py")
+shelf_path = os.path.expanduser(r"~\\Documents\\maya\\2018\\prefs\\shelves\\shelf_Backspace.mel")
 
 usersetup_content = '''
 import sys
 import site
 
-sys.path.append('{scripts_path}')
+sys.path.append('{pipe_path}')
 site.addsitedir('{venv_path}')
-'''.format(scripts_path=scripts_path, venv_path=venv_path)
+'''.format(pipe_path=pipe_path, venv_path=venv_path)
 
 def usersetup():
-	print("\n##################################")
-	print("## Installing Backspace Pipe... ")
-
+	print("## Checking User Setup...")
 	if os.path.exists(usersetup_path) and os.stat(usersetup_path).st_size != 0:
 		print("# existing usersetup.py found!")
 
@@ -36,9 +37,21 @@ def usersetup():
 		with open(usersetup_path, "a") as f:
 			f.write(usersetup_content)
 
-	print("## Installation Complete! ")
-	print("##################################")
-	time.sleep(5)
+def shelves():
+	print("## Installing Backspace Shelf...")
+	if os.path.exists(shelf_path):
+		print("# Replacing Backspace Shelf")
+		shutil.copy(pipe_path + "\\shelves\\shelf_Backspace.mel", shelf_path)
+	else:
+		shutil.copy(pipe_path + "\\shelves\\shelf_Backspace.mel", shelf_path)
+
+
 
 if __name__ == "__main__":
+	print("\n##################################")
+	print("### Installing Backspace Pipe... ")
 	usersetup()
+	shelves()
+	print("### Installation Complete! ")
+	print("##################################")
+	time.sleep(5)
